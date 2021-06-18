ad141c699b479628f2a0ec34e6829d157e18b30a
Jesus Parejo <japarejo.diaz@gmail.com>
2021-06-18 19:50:04 -0300

    primera subida al repo local CRUD basico java

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* Populate Table clientes
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jesus', 'Parejo', 'jparejo@gmail.com', '2020-01-01' );
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jose', 'Parejo', 'jparejo@gmail.com', '2020-01-02' );
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Josue', 'Parejo', 'jparejo@gmail.com', '2020-01-02' );
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Juan', 'Parejo', 'jparejo@gmail.com', '2020-01-04' );
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jeremias', 'Parejo', 'jparejo@gmail.com', '2020-01-05');
